9699ddfb13bb24f0924780924c0069445a19f961
Ryan Siu <rsiu@stuy.edu>
2017-12-31 15:45:44 -0500

    Add temporary public home page

--- main.py
+++ main.py
@@ -5,7 +5,15 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.debug = True
--- utils/db.py
+++ utils/db.py
@@ -3,41 +3,41 @@ import hashlib   # allows for passwords and emails to be encrypted and decrypted
 
 f = "data/database.db"
 
-def openDB():
+def open_db():
     db = sqlite3.connect(f) # open if f exists, otherwise create
     c = db.cursor()         # facilitate db ops
     return db, c
 
-def closeDB(db):
+def close_db(db):
     db.commit()
     db.close()
 
-def createTables():
-    db, c = openDB()
+def create_tables():
+    db, c = open_db()
     c.execute("create table Users(user_id integer PRIMARY KEY, password TEXT NOT NULL, admin BOOLEAN, name TEXT, email TEXT)")
     c.execute("create table Items(item_id integer PRIMARY KEY, item_name TEXT NOT NULL, price REAL, description TEXT, status INTEGER, is_selling BOOLEAN, user_id integer)")
     c.execute("create table Pictures(picture_id PRIMARY KEY, item_id integer, path TEXT)")
-    closeDB(db)
+    close_db(db)
 
-def authUser():
-    db, c = openDB()
+def auth_user():
+    db, c = open_db()
     command = "SELECT * FROM Users where username = '%s' AND password = '%s'" % (username, hashlib.md5(str(password)).hexdigest())
     user = ''
     for user in c.execute(command): # returns either 1 or 0 entries
         pass # sets user to the entry if it exists
-    closeDB(db)
+    close_db(db)
     if user: # checks if user is an empty string
         return True # 1 result
     return False    # no results
     
-def addUser();
+def add_user():
     pass
     
-def addItem();
+def add_item():
     pass
     
-def changeItem();
+def change_item():
     pass
     
-def addPicture();
+def add_picture():
     pass
